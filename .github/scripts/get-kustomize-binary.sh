3e7901765d791dc1bc4ce5269e0a15b1b045fc5a
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-10-31 23:07:41 +0100

    feat: refactor get-kustomize-binary script to use reusable functions for downloading binaries

--- .github/scripts/get-kustomize-binary.sh
+++ .github/scripts/get-kustomize-binary.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 set -e
 
-curl -s "https://api.github.com/repos/kubernetes-sigs/kustomize/releases" | grep "browser_download.*darwin_amd64" | cut -d '"' -f 4 | sort -V | tail -n 1 | xargs curl -LJ | tar xvz -C src/Devantler.KustomizeCLI/assets/binaries kustomize
-mv src/Devantler.KustomizeCLI/assets/binaries/kustomize src/Devantler.KustomizeCLI/assets/binaries/kustomize-darwin-amd64
-chmod +x src/Devantler.KustomizeCLI/assets/binaries/kustomize-darwin-amd64
-curl -s "https://api.github.com/repos/kubernetes-sigs/kustomize/releases" | grep "browser_download.*darwin_arm64" | cut -d '"' -f 4 | sort -V | tail -n 1 | xargs curl -LJ | tar xvz -C src/Devantler.KustomizeCLI/assets/binaries kustomize
-mv src/Devantler.KustomizeCLI/assets/binaries/kustomize src/Devantler.KustomizeCLI/assets/binaries/kustomize-darwin-arm64
-chmod +x src/Devantler.KustomizeCLI/assets/binaries/kustomize-darwin-arm64
-curl -s "https://api.github.com/repos/kubernetes-sigs/kustomize/releases" | grep "browser_download.*linux_amd64" | cut -d '"' -f 4 | sort -V | tail -n 1 | xargs curl -LJ | tar xvz -C src/Devantler.KustomizeCLI/assets/binaries kustomize
-mv src/Devantler.KustomizeCLI/assets/binaries/kustomize src/Devantler.KustomizeCLI/assets/binaries/kustomize-linux-amd64
-chmod +x src/Devantler.KustomizeCLI/assets/binaries/kustomize-linux-amd64
-curl -s "https://api.github.com/repos/kubernetes-sigs/kustomize/releases" | grep "browser_download.*linux_arm64" | cut -d '"' -f 4 | sort -V | tail -n 1 | xargs curl -LJ | tar xvz -C src/Devantler.KustomizeCLI/assets/binaries kustomize
-mv src/Devantler.KustomizeCLI/assets/binaries/kustomize src/Devantler.KustomizeCLI/assets/binaries/kustomize-linux-arm64
-chmod +x src/Devantler.KustomizeCLI/assets/binaries/kustomize-linux-arm64
+get_latest_release() {
+  local repo=$1
+  local os=$2
+  local arch=$3
+  curl --silent "https://api.github.com/repos/$repo/releases/latest" |
+    grep '"name":' |
+    grep "$os" |
+    grep "$arch" |
+    sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+get() {
+  local repo=$1
+  local os=$2
+  local arch=$3
+  local binary=$4
+  local target_dir=$5
+  local target_name=$6
+  local archiveType=$7
+  latest_release=$(get_latest_release "$repo" "$os" "$arch")
+  version=$(echo "$latest_release" | sed -E 's/.*_(v[0-9]+\.[0-9]+\.[0-9]+).*/\1/')
+  local url="https://github.com/$repo/releases/download/$binary/$version/$latest_release"
+
+  echo "Downloading $target_name from $url"
+  if [ "$archiveType" = "tar" ]; then
+    curl -LJ "$url" | tar xvz -C "$target_dir" "$binary"
+    mv "$target_dir/$binary" "${target_dir}/$target_name"
+  elif [ "$archiveType" = "zip" ]; then
+    curl -LJ "$url" -o "$target_dir/$target_name.zip"
+    unzip -o "$target_dir/$target_name.zip" -d "$target_dir"
+    mv "$target_dir/$binary" "${target_dir}/$target_name"
+    rm "$target_dir/$target_name.zip"
+  elif [ "$archiveType" = false ]; then
+    curl -LJ "$url" -o "$target_dir/$target_name"
+  fi
+  chmod +x "$target_dir/$target_name"
+}
+
+repo="kubernetes-sigs/kustomize"
+
+get "$repo" "darwin" "arm64" "kustomize" "Devantler.KustomizeCLI/runtimes/osx-arm64/native" "kustomize-osx-arm64" "tar"
+get "$repo" "darwin" "amd64" "kustomize" "Devantler.KustomizeCLI/runtimes/osx-x64/native" "kustomize-osx-x64" "tar"
+get "$repo" "linux" "arm64" "kustomize" "Devantler.KustomizeCLI/runtimes/linux-arm64/native" "kustomize-linux-arm64" "tar"
+get "$repo" "linux" "amd64" "kustomize" "Devantler.KustomizeCLI/runtimes/linux-x64/native" "kustomize-linux-x64" "tar"
+get "$repo" "windows" "amd64" "kustomize.exe" "Devantler.KustomizeCLI/runtimes/win-x64/native" "kustomize-win-x64.exe" "zip"
+get "$repo" "windows" "arm64" "kustomize.exe" "Devantler.KustomizeCLI/runtimes/win-arm64/native" "kustomize-win-arm64.exe" "zip"
