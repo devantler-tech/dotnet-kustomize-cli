c3dff07db275248beaf950d126e551ac59f6eac0
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-10-31 22:21:11 +0100

    feat: add initial implementation of .NET Kustomize CLI with tests and GitHub Actions workflow

---
+++ Devantler.KustomizeCLI.Tests/KustomizeTests/GetCommandTests.cs
@@ -0,0 +1,46 @@
+using System.Runtime.InteropServices;
+
+namespace Devantler.KustomizeCLI.Tests.KustomizeTests;
+
+/// <summary>
+/// Tests for the <see cref="Kustomize.GetCommand(PlatformID?, Architecture?, string?)"/> method.
+/// </summary>
+public class GetCommandTests
+{
+  /// <summary>
+  /// Test to verify that the command returns the correct binary for MacOS on x64 architecture.
+  /// </summary>
+  [Theory]
+  [InlineData(PlatformID.Unix, Architecture.X64, "osx-x64", "kustomize-osx-x64")]
+  [InlineData(PlatformID.Unix, Architecture.Arm64, "osx-arm64", "kustomize-osx-arm64")]
+  [InlineData(PlatformID.Unix, Architecture.X64, "linux-x64", "kustomize-linux-x64")]
+  [InlineData(PlatformID.Unix, Architecture.Arm64, "linux-arm64", "kustomize-linux-arm64")]
+  [InlineData(PlatformID.Win32NT, Architecture.X64, "win-x64", "kustomize-win-x64.exe")]
+  [InlineData(PlatformID.Win32NT, Architecture.X64, "win-arm64", "kustomize-win-arm64.exe")]
+  public void GetCommand_ShouldReturnOSXx64Binary(PlatformID platformID, Architecture architecture, string runtimeIdentifier, string expectedBinary)
+  {
+    // Act
+    string actualBinary = Path.GetFileName(Kustomize.GetCommand(platformID, architecture, runtimeIdentifier).TargetFilePath);
+
+    // Assert
+    Assert.Equal(expectedBinary, actualBinary);
+  }
+
+  /// <summary>
+  /// Test to verify that the command returns a <see cref="PlatformNotSupportedException"/> when the platform is not supported.
+  /// </summary>
+  [Fact]
+  public void GetCommand_GivenInvaldiPlatform_ShouldThrowPlatformNotSupportedException()
+  {
+    // Arrange
+    var platformID = PlatformID.Other;
+    var architecture = Architecture.Wasm;
+    string runtimeIdentifier = "wasm";
+
+    // Act
+    void Act() => Kustomize.GetCommand(platformID, architecture, runtimeIdentifier);
+
+    // Assert
+    _ = Assert.Throws<PlatformNotSupportedException>(Act);
+  }
+}
--- README.md
+++ README.md
@@ -1,16 +1,71 @@
 # Ⓚ .NET Kustomize CLI
 
 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Test](https://github.com/devantler/dotnet-kustomize-cli/actions/workflows/test.yaml/badge.svg)](https://github.com/devantler/dotnet-kustomize-cli/actions/workflows/test.yaml)
+[![codecov](https://codecov.io/gh/devantler/dotnet-kustomize-cli/graph/badge.svg?token=RhQPb4fE7z)](https://codecov.io/gh/devantler/dotnet-kustomize-cli)
+
+<details>
+  <summary>Show/hide folder structure</summary>
 
 <!-- readme-tree start -->
+
 ```
 .
 ├── .github
 │   ├── scripts
 │   └── workflows
-├── src
-└── tests
+├── Devantler.KustomizeCLI
+│   └── runtimes
+│       ├── linux-arm64
+│       │   └── native
+│       ├── linux-x64
+│       │   └── native
+│       ├── osx-arm64
+│       │   └── native
+│       ├── osx-x64
+│       │   └── native
+│       ├── win-arm64
+│       │   └── native
+│       └── win-x64
+│           └── native
+└── Devantler.KustomizeCLI.Tests
+    ├── KustomizeTests
+    └── assets
+        └── k8s
+            ├── apps
+            ├── clusters
+            │   └── ksail-default
+            │       └── flux-system
+            └── infrastructure
+                └── controllers
 
-5 directories
+27 directories
 ```
+
 <!-- readme-tree end -->
+
+</details>
+
+A simple .NET library that embeds the Kustomize CLI.
+
+## 🚀 Getting Started
+
+To get started, you can install the package from NuGet.
+
+```bash
+dotnet add package Devantler.KustomizeCLI
+```
+
+## 📝 Usage
+
+You can execute the Kustomize CLI commands using the `Kustomize` class.
+
+```csharp
+using Devantler.KustomizeCLI;
+
+var cmd = Kustomize.Command;
+
+cmd.WithArguments("build", "path/to/kustomization");
+
+await CLI.RunAsync(cmd, CancellationToken.None);
+```
